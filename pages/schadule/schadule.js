7198fda8ccabe47fe5c0e287952e1e5ea5e041f3
盛耀宇 <491578592@163.com>
2018-04-16 22:53:11 +0800

    Update schadule.js

--- pages/schadule/schadule.js
+++ pages/schadule/schadule.js
@@ -8,10 +8,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD","#FF0000","#888888","#0000FF"],//课标颜色样式
+    colorArrays: ["#85B8CF", "#90C652", "#D8AA5A", "#FC9F9D", "#0A9A84", "#61BC69", "#12AEF3", "#E29AAD","#FF0000","#618888","#0000FF"],//课标颜色样式
     weekArrays: ["第1周", "第2周", "第3周", "第4周", "第5周", "第6周", "第7周", "第8周", "第9周", "第10周", "第11周", "第12周", "第13周", "第14周", "第15周", "第16周", "第17周", "第18周", "第19周","第20周"],
     week: 0,//显示第几周的课表
-    thisWeek: 2,//当前周
+    thisWeek: 9,//当前周
     _userInfo: [],//学生信息
     classInfo_c:[],//课表信息
     classInfo_e: [
@@ -40,46 +40,77 @@ Page({
     schaduleArray: []//课表
   },
 
+  /** 
+   *
+   */
   onLoad: function () {
     console.log("1.onload");
     var that = this;
   },
 
+  /** 
+   * 课表生成函数
+   */
   changeSchadule: function () {
     var that = this;
     var colorNumber = 0;//课程颜色
+    var weekArray = new Array();
 
     for (var m = 0; m < that.data.classInfo_c.length - 1; m++) {
+      
       that.data.classInfo_e[m].kcm = that.data.classInfo_c[m].课程名;
+      that.data.classInfo_e[m].zc = that.data.classInfo_c[m].周次;
+      that.data.classInfo_e[m].js = that.data.classInfo_c[m].教室;
+      that.data.classInfo_e[m].xq = that.data.classInfo_c[m].星期;
+      that.data.classInfo_e[m].ls = that.data.classInfo_c[m].老师;
+      that.data.classInfo_e[m].jc = that.data.classInfo_c[m].节次;
+      that.data.classInfo_e[m].kcdm = that.data.classInfo_c[m].课程代码;
+
       that.data.classInfo_e[m].color = that.data.colorArrays[colorNumber++];
-
-      /* 课程从第x周开始 */
-      var str = that.data.classInfo_c[m].周次 + "";
-      var x = parseInt(str[0]);
-
-      if (x > that.data.week + 1) {
-        that.data.classInfo_e[m].color = "grey";
-      } else {
-        for (var j = 0; j < m; j++) {
-          if (that.data.classInfo_e[m].kcm == that.data.classInfo_e[j].kcm) {
-            that.data.classInfo_e[m].color = that.data.classInfo_e[j].color;
-            colorNumber--;
+      //相同的课显示相同的颜色
+      for (var j = 0; j < m; j++) {
+        if (that.data.classInfo_e[m].kcm == that.data.classInfo_e[j].kcm) {
+          that.data.classInfo_e[m].color = that.data.classInfo_e[j].color;
+          colorNumber--;
+        }
+      }
+      
+      /* 未开始的课的颜色 */
+      var str = (that.data.classInfo_c[m].周次 + "").replace("\(周\)", "").replace("\(周\)", "").replace("\(周\)", "");
+      weekArray = str.split(",");
+      var color = that.data.classInfo_e[m].color;
+      that.data.classInfo_e[m].color = "grey"
+      for(var i = 0; i < weekArray.length; i++) {
+        var weekArr = weekArray[i].split("-");
+        if(weekArr.length == 1) {
+          if(that.data.week + 1 == weekArr[0]) {
+            that.data.classInfo_e[m].color = color;
+          }
+        } else {
+          if(that.data.week + 1 >= weekArr[0] && that.data.week + 1 <= weekArr[1]) {
+            that.data.classInfo_e[m].color = color;
           }
         }
       }
-      console.log(that.data.classInfo_e[m].color);
     }
     for (var n = 0; n < that.data.classInfo_c.length - 1; n++) {
 
       //以课程的节次和星期组成二维数组储存对应课表的位置
       var i = parseInt((that.data.classInfo_c[n].节次 - 1) + "" + (that.data.classInfo_c[n].星期 - 1));
-      that.data.schaduleArray[i] = n;
 
-      /* 课程从第y周结束 */
-      var str = that.data.classInfo_c[n].周次 + "";
-      var y = parseInt(str.substr(2, 2));
-      if (y < that.data.week + 1) {
-        that.data.schaduleArray[i] = that.data.classInfo_e.length - 1;
+      /* 课程安排 */
+      //课程结束后删除该课程
+      var str = (that.data.classInfo_c[n].周次 + "").replace("\(周\)", "").replace("\(周\)", "").replace("\(周\)", "");
+      weekArray = str.split(",");
+      var weekArr = weekArray[weekArray.length-1].split("-");
+      if (weekArr[weekArr.length - 1] < that.data.week + 1) {
+        if (!that.data.schaduleArray[i]) {
+          that.data.schaduleArray[i] = that.data.classInfo_e.length - 1;
+        }
+      } else {
+
+        //添加课程
+        that.data.schaduleArray[i] = n;
       }
 
       //使相连的两节同样的课程只显示一次
@@ -87,12 +118,16 @@ Page({
         that.data.classInfo_e[n].kcm = "";
       }
     }
+
     that.setData({
       classInfo_e: that.data.classInfo_e,
       schaduleArray: that.data.schaduleArray
     });
   },
 
+  /** 
+   *
+   */
   onShow: function () {
     console.log("1.onShow");
     var schaduleArray = new Array();
@@ -114,23 +149,8 @@ Page({
             classInfo_c: res.data.课表
           });
 
+          //生成课表
           that.changeSchadule();
-          /*
-          for (var n = 0; n < that.data.classInfo.length; n++) {
-            var idx = 0;
-            var str = that.data.classInfo[n].周次 + "";
-            var i = parseInt(str[0]);
-            var j = parseInt(str.substr(2, 2));
-            for (var w = i - 1; w < 1; w++) {
-              schaduleArray[w] = new Array();
-              schaduleArray[w][that.data.classInfo[n].节次] = new Array();
-              schaduleArray[w][that.data.classInfo[n].节次][that.data.classInfo[n].星期] = idx;
-              console.log(that.data.classInfo[0].节次===1);
-              console.log(that.data.classInfo[0].星期===2);
-              console.log(schaduleArray[0][1][2])
-            };
-            idx++;
-          }*/
         },
         fail: function (res) { },
         complete: function (res) { },
@@ -138,6 +158,9 @@ Page({
     }
   },
 
+  /** 
+   *
+   */
   bindPickerChange: function (e) {
     this.setData({
       week: e.detail.value
@@ -145,6 +168,9 @@ Page({
     this.changeSchadule();
   },
   
+  /** 
+   *
+   */
   BackToThisWeek: function () {
     this.setData({
       week: this.data.thisWeek
